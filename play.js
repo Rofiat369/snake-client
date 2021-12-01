6ff5103fc3ef34d906d970dcdc43d3e839ce5231
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-12-01 02:08:24 +0000

    created a new file and moved the connect function into it

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here,
-    port: 50541// PORT number here,
-  });
+const connect = require("./client")
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("You ded cos you idled");
-  });
-
-  return conn;
-};
 
 
 
